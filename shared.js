3c772fd77430fe628bd3d7c7de568aeed2994616
Sweet-ka <sweet-ka@mail.ru>
2023-05-07 13:02:12 +0300

    fix: exports shared

--- shared.js
+++ shared.js
@@ -1,46 +1,69 @@
 const fs = require("fs/promises");
 const path = require("path");
 
+async function read(root, dirName, fileName) {
+  return fs.readFile(path.join(root, dirName, fileName), "utf8", function (error) {
+    if (error) throw error;
+  });
+}
+
+async function apend(root, dirName, fileName, str) {
+  return fs.appendFile(path.join(root, dirName, fileName), str + "\n", (error) => {
+    if (error) throw error;
+  });
+}
+
+async function getFiles(root, dirName) {
+  return fs.readdir(path.join(root, dirName), (error) => {
+    if (error) throw error;
+  });
+}
+
+async function getStats(root, dirName, fileName) {
+  return fs.stat(path.join(root, dirName, fileName), (error) => {
+    if (error) throw error;
+  });
+}
+
+async function createFile(root, dirName, fileName, flag, template) {
+  return fs.open(path.join(root, dirName, fileName), flag, template, (error) => {
+    if (error) throw error;
+  });
+}
+
+async function createDir(root, dirName, recursive) {
+  return fs.mkdir(path.join(root, dirName), { recursive: recursive }, (error) => {
+    if (error) throw error;
+  });
+}
+
+async function copyFiles(file, newFile, template) {
+  await fs.copyFile(file, newFile, template, (error) => {
+    if (error) throw error;
+  });
+}
+
+async function copyExt(root, distFolder, folderName, fileName, ext) {
+  const fileOpened = await createFile(root, distFolder, fileName, "w");
+  const innerFiles = await getFiles(root, folderName);
+
+  for (let file of innerFiles) {
+    const statsFile = await getStats(root, folderName, file);
+    if (statsFile.isFile() && path.extname(file) === `.${ext}`) {
+      const data = await read(root, folderName, file);
+      await apend(root, distFolder, fileName, data);
+    }
+  }
+  fileOpened.close();
+}
+
 module.exports = {
-  read: async function (root, dirName, fileName) {
-    return fs.readFile(path.join(root, dirName, fileName), "utf8", function (error) {
-      if (error) throw error;
-    });
-  },
-
-  apend: async function (root, dirName, fileName, str) {
-    return fs.appendFile(path.join(root, dirName, fileName), str + "\n", (error) => {
-      if (error) throw error;
-    });
-  },
-
-  getFiles: async function (root, dirName) {
-    return fs.readdir(path.join(root, dirName), (error) => {
-      if (error) throw error;
-    });
-  },
-
-  getStats: async function (root, dirName, fileName) {
-    return fs.stat(path.join(root, dirName, fileName), (error) => {
-      if (error) throw error;
-    });
-  },
-
-  createFile: async function (root, dirName, fileName, flag, template) {
-    return fs.open(path.join(root, dirName, fileName), flag, template, (error) => {
-      if (error) throw error;
-    });
-  },
-
-  createDir: async function (root, dirName, recursive) {
-    return fs.mkdir(path.join(root, dirName), { recursive: recursive }, (error) => {
-      if (error) throw error;
-    });
-  },
-
-  copyFiles: async function (file, newFile, template) {
-    await fs.copyFile(file, newFile, template, (error) => {
-      if (error) throw error;
-    });
-  },
+  read: read,
+  apend: apend,
+  getFiles: getFiles,
+  getStats: getStats,
+  createFile: createFile,
+  createDir: createDir,
+  copyFiles: copyFiles,
+  copyExt: copyExt,
 };
