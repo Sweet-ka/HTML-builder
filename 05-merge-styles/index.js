54b3b81f34ff44668d409ca6d55ac5ac1250fd04
Sweet-ka <sweet-ka@mail.ru>
2023-05-07 13:02:37 +0300

    fix: function to shared

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,21 +1,6 @@
-const path = require("path");
-const { getStats, apend, getFiles, createFile, read } = require("../shared");
+const { copyExt } = require("../shared");
 const folderName = "styles";
 const fileName = "bundle.css";
 const distFolder = "project-dist";
 
-copyStyles(folderName, fileName);
-
-async function copyStyles(dir, fileName) {
-  const fileOpened = await createFile(__dirname, distFolder, fileName, "w");
-  const innerFiles = await getFiles(__dirname, dir);
-
-  for (let file of innerFiles) {
-    const statsFile = await getStats(__dirname, folderName, file);
-    if (statsFile.isFile() && path.extname(file) === ".css") {
-      const data = await read(__dirname, folderName, file);
-      await apend(__dirname, distFolder, fileName, data);
-    }
-  }
-  fileOpened.close();
-}
+copyExt(__dirname, distFolder, folderName, fileName, "css");
